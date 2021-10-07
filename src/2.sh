4c99d790a95d37bb2b9ac9207e2439eb9d85baed
ytannai <54846100+ytannai@users.noreply.github.com>
2021-10-07 21:26:44 +0900

    fix: SC2012: Use find instead of ls to better handle non-alphanumeric filenames.

--- src/2.sh
+++ src/2.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-time ls src/data/2/img/*.png | sed 's/\.png$//' | xargs -I{} convert {}.png {}.jpg
+time find src/data/2/img -maxdepth 1 -type f -name "*.png" | sed 's/\.png$//' | xargs -I{} convert {}.png {}.jpg
