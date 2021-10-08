b011b11cdfaaf41217f46431e590dddb7fd2d4de
ytannai <54846100+ytannai@users.noreply.github.com>
2021-10-08 21:48:08 +0900

    xargsの並列対応

--- src/2.sh
+++ src/2.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-time find src/data/2/img -maxdepth 1 -type f -name "*.png" | sed 's/\.png$//' | xargs -I{} convert {}.png {}.jpg
+time find src/data/2/img -maxdepth 1 -type f -name "*.png" | sed 's/\.png$//' | xargs -P"$(nproc)" -I{} convert {}.png {}.jpg
